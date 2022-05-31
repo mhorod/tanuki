ad16c35295521fb6c5689cb0a5db37ddb854e6dc
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-05-31 21:25:15 +0200

    basic submission system

---
+++ app/src/source.ts
@@ -0,0 +1,37 @@
+import { SourceManager } from "./submit.ts"
+import { join, readAll } from "../deps.ts"
+// Directory where submited files go
+const SUBMIT_DIR = '/app/files/submitted'
+
+class BasicSourceManager implements SourceManager {
+    constructor() {
+        try {
+            Deno.mkdir(SUBMIT_DIR, { recursive: true })
+        }
+        catch {
+            //  Directory already exists, skip
+        }
+    }
+
+    async addSource(uri: string, content: any): Promise<boolean> {
+        try {
+            await Deno.writeFile(join(SUBMIT_DIR, uri), content);
+            return true;
+        }
+        catch {
+            return false;
+        }
+    }
+
+    async loadSource(uri: string): Promise<string | null> {
+        let file;
+        try { file = await Deno.open(join(SUBMIT_DIR, uri)); }
+        catch { return null; }
+
+        const decoder = new TextDecoder('utf-8');
+        const text = decoder.decode(await readAll(file));
+        return text;
+    }
+}
+
+export { BasicSourceManager }
--- app/src/submit.ts
+++ app/src/submit.ts
@@ -1,6 +1,11 @@
 
-import { MultipartReader, exists, R } from "../deps.ts"
+import { MultipartReader, R } from "../deps.ts"
 import type { IRouter, OpineRequest, OpineResponse, NextFunction } from "../deps.ts"
+import { format } from "../deps.ts"
+import { RequestAuthenticator, authorizeUsing } from "./auth.ts"
+import { renderWithUserData } from "./utils.ts"
+import { SubmitDB } from "./db.ts"
+
 
 const { compose, nth, split } = R;
 const TMP_DIR = '/app/public/uploads'
@@ -16,27 +21,80 @@ async function upload(req: OpineRequest) {
     if (contentType.startsWith('multipart/form-data'))
         boundary = getBoundary(contentType);
 
-    console.log(boundary);
-    if (!(await exists(TMP_DIR)))
-        await Deno.mkdir(TMP_DIR, { recursive: true })
     const form = await new MultipartReader(req.body, boundary).readForm({ maxMemory: 10 << 20, dir: TMP_DIR })
 
     return form;
 }
 
-export function handleSubmits(router: IRouter) {
-    router.get("/submit", (req, res, next) => {
-        res.render("submit")
-    })
+
+interface SourceManager {
+    /**
+     * Add new source file to database
+     * @returns Promise with boolean value that is true when operation succeeded
+     */
+    addSource(uri: string, source: any): Promise<boolean>;
+
+    /**
+     * Load source file from given uri
+     * @returns Promise with file content, or null if operation failed
+     */
+    loadSource(uri: string): Promise<string | null>;
+
+}
+
+interface SubmitRouterConfig {
+    authenticator: RequestAuthenticator,
+    submitDB: SubmitDB,
+    sourceManager: SourceManager
+}
+
+function setUpSubmitRouter(router: IRouter, config: SubmitRouterConfig) {
+    router.get("/submit",
+        authorizeUsing(config.authenticator, _ => true),
+        renderWithUserData(config.authenticator, "submit")
+    );
 
     router.post("/submit", async (req, res, next) => {
         const files = (await upload(req)).files('source');
         if (files != undefined) {
             const file = files[0];
-            console.log(file);
-            if (file.content != undefined)
-                Deno.writeFile("/app/public/uploads/0.png", file.content);
-            res.render("submit-results", { src: "/uploads/0.png" })
+            // TODO: do something smarter about that
+            if (file.content == undefined)
+                res.redirect("/");
+
+            const uri = crypto.randomUUID();
+            const added = await config.sourceManager.addSource(uri, file.content)
+            if (!added)
+                res.redirect("/")
+            const submit = await config.submitDB.addSubmit(0, 0, uri);
+            if (submit == null)
+                res.redirect("/")
+            else
+                res.redirect("/results/" + submit.id.toString())
         }
     })
+
+    router.get("/results/:submit_id",
+        async (req, res, next) => {
+            const submit = await config.submitDB.getSubmitById(parseInt(req.params.submit_id));
+            if (submit == null) {
+                res.sendStatus(404);
+                return;
+            }
+            const src = await config.sourceManager.loadSource(submit.source);
+            renderWithUserData(config.authenticator, "results",
+                {
+                    submit: req.params.submit_id,
+                    contest: "Kurs obsługi dziurkacza",
+                    problem: "A",
+                    status: "OK",
+                    src: src,
+                    date: format(submit.date, "yyyy-MM-dd, hh:mm:ss")
+                }
+            )(req, res, next);
+        }
+    )
 }
+
+export { setUpSubmitRouter }
+export type { SourceManager }
