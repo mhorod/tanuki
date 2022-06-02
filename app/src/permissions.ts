a76d322d8599272c22c19ec3f7713a7c343dce41
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-06-03 00:21:31 +0200

    created permission db interface and mock

---
+++ app/src/permissions.ts
@@ -0,0 +1,21 @@
+
+enum PermissionKind {
+    MANAGE,
+    SUBMIT,
+}
+
+interface Permission {
+    id: number,
+    kind: PermissionKind,
+    description: string | null,
+}
+
+
+interface PermissionDB {
+    canSubmit(user: number, contest: number): Promise<boolean>;
+    canViewContest(user: number, contest: number): Promise<boolean>;
+    canViewSubmit(user: number, submit: number): Promise<boolean>;
+}
+
+export { PermissionKind };
+export type { Permission, PermissionDB };
