5ff7ab49df5d73889225e8fc6b8ef9362b6b6ba5
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-06-03 21:57:31 +0200

    moved id to the end so it's set automatically

--- app/db/0_create_schema.sql
+++ app/db/0_create_schema.sql
@@ -23,47 +23,47 @@ CREATE SEQUENCE tanuki.tasks_id_seq START WITH 1 INCREMENT BY 1;
 CREATE SEQUENCE tanuki.users_id_seq START WITH 1 INCREMENT BY 1;
 
 CREATE  TABLE tanuki.contests ( 
-	id                   serial  NOT NULL  ,
 	name                 varchar(64)  NOT NULL  ,
 	shortname            varchar(16)    ,
 	is_active            boolean  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_contests PRIMARY KEY ( id )
  );
 
 CREATE  TABLE tanuki.languages ( 
-	id                   serial  NOT NULL  ,
 	name                 varchar(50)  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_languages PRIMARY KEY ( id )
  );
 
 CREATE  TABLE tanuki.permissions_for_contests ( 
-	id                   serial  NOT NULL  ,
 	name                 varchar(64)  NOT NULL  ,
 	description          varchar(256)    ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_tbl PRIMARY KEY ( id )
  );
 
 CREATE  TABLE tanuki.scoring_methods ( 
-	id                   serial  NOT NULL  ,
 	name                 varchar(32)    ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT idx_scoring_methods PRIMARY KEY ( id )
  );
 
 CREATE  TABLE tanuki.statuses ( 
-	id                   serial  NOT NULL  ,
 	name                 char(5)  NOT NULL  ,
 	description          varchar(128)    ,
 	priority             integer  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_statuses PRIMARY KEY ( id )
  );
 
 CREATE  TABLE tanuki.users ( 
-	id                   serial  NOT NULL  ,
 	login                char(32)  NOT NULL  ,
 	name                 varchar(64)  NOT NULL  ,
 	surname              varchar(64)  NOT NULL  ,
 	password_hash        char(256)  NOT NULL  ,
 	email                varchar  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_users PRIMARY KEY ( id ),
 	CONSTRAINT unique_login UNIQUE ( login ) 
  );
@@ -91,7 +91,6 @@ CREATE  TABLE tanuki.extensions (
  );
 
 CREATE  TABLE tanuki.problems ( 
-	id                   serial  NOT NULL  ,
 	name                 varchar(64)  NOT NULL  ,
 	shortname            varchar(16)  NOT NULL  ,
 	contest_id           integer  NOT NULL  ,
@@ -104,18 +103,19 @@ CREATE  TABLE tanuki.problems (
 	published            boolean  NOT NULL  ,
 	scoring_method       integer    ,
 	source_limit         integer    ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_problems PRIMARY KEY ( id ),
 	CONSTRAINT contest_id FOREIGN KEY ( contest_id ) REFERENCES tanuki.contests( id )   ,
 	CONSTRAINT fk_problems_scoring_methods FOREIGN KEY ( scoring_method ) REFERENCES tanuki.scoring_methods( id )   
  );
 
 CREATE  TABLE tanuki.submits ( 
-	id                   serial  NOT NULL  ,
 	source_uri           char(128)  NOT NULL  ,
 	user_id              integer  NOT NULL  ,
 	problem_id           integer  NOT NULL  ,
 	language_id          integer    ,
 	submission_time      date DEFAULT ('now'::text)::date NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_submits PRIMARY KEY ( id ),
 	CONSTRAINT fk_submits_languages FOREIGN KEY ( language_id ) REFERENCES tanuki.languages( id )   ,
 	CONSTRAINT problem_id FOREIGN KEY ( problem_id ) REFERENCES tanuki.problems( id )   ,
@@ -123,22 +123,22 @@ CREATE  TABLE tanuki.submits (
  );
 
 CREATE  TABLE tanuki.task_groups ( 
-	id                   serial  NOT NULL  ,
 	problem_id           integer  NOT NULL  ,
 	name                 varchar(32)  NOT NULL  ,
 	requires_full_points boolean  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_tasks PRIMARY KEY ( id ),
 	CONSTRAINT problem_id FOREIGN KEY ( problem_id ) REFERENCES tanuki.problems( id )   
  );
 
 CREATE  TABLE tanuki.tasks ( 
-	id                   serial  NOT NULL  ,
 	task_group           integer  NOT NULL  ,
 	test_uri             varchar(256)  NOT NULL  ,
 	points               numeric  NOT NULL  ,
 	time_limit           numeric  NOT NULL  ,
 	memory_limit         integer  NOT NULL  ,
 	show_output          boolean  NOT NULL  ,
+	id                   serial  NOT NULL  ,
 	CONSTRAINT pk_tasks_0 PRIMARY KEY ( id ),
 	CONSTRAINT task_group_id FOREIGN KEY ( task_group ) REFERENCES tanuki.task_groups( id )   
  );
@@ -207,108 +207,108 @@ COMMENT ON COLUMN tanuki.task_results.execution_time IS 'execution time in secon
 COMMENT ON COLUMN tanuki.task_results.used_memory IS 'used memory (in bytes)';
 
 SET search_path TO tanuki;
-INSERT INTO languages VALUES (0, 'Fortran');
-INSERT INTO languages VALUES (1, 'C++');
-INSERT INTO languages VALUES (2, 'Java');
+INSERT INTO languages VALUES ('Fortran');
+INSERT INTO languages VALUES ('C++');
+INSERT INTO languages VALUES ('Java');
 
-INSERT INTO extensions VALUES ('f', 0);
-INSERT INTO extensions VALUES ('cpp', 1);
-INSERT INTO extensions VALUES ('hpp', 1);
-INSERT INTO extensions VALUES ('h', 1);
-INSERT INTO extensions VALUES ('java', 2);
+INSERT INTO extensions VALUES ('f', 1);
+INSERT INTO extensions VALUES ('cpp', 2);
+INSERT INTO extensions VALUES ('hpp', 2);
+INSERT INTO extensions VALUES ('h', 2);
+INSERT INTO extensions VALUES ('java', 3);
 
-INSERT INTO users VALUES (0, 'admin', 'Jowita', 'Szef', '6f7900d309a6ed96c4677c81eedc3f6c', 'jowita.szef@gmail.com');
-INSERT INTO users VALUES (1, 'super_prowadzacy', 'Bartosz', 'Algorytm', '9e301dfc77aeac8e3b6fc4c8fe28607d', 'bartosz.algorytm@[123.123.123.123]');
-INSERT INTO users VALUES (2, 'niesuper_prowadzacy', 'Tycjan', 'Koder', 'd350d14e52114698fbf5206f9ae7e4d2', 'tycjan"()[]"koder-@domena');
-INSERT INTO users VALUES (3, 'PilnyStudent', 'Evialina', 'Okejka', 'd7f3ec4fe96072168e6b39caae9705d6', 'ev2001@fajnapoczta.pl');
-INSERT INTO users VALUES (4, 'pijepiwozamiastsieuczyc', 'Sergiusz', 'Znowuans', 'f12cbf3fe366d7f02304b5aff794a1ee', 'serg.ans@fajnapoczta.pl');
-INSERT INTO users VALUES (5, 'I<3Mexico', 'Pablo Diego José Francisco', 'de Paula Juan Nepomuceno María de los Remedios Cipriano', '87bccbacffc1592e88f855b16f73faba', 'pablo@correos.mx');
+INSERT INTO users VALUES ('admin', 'Jowita', 'Szef', '6f7900d309a6ed96c4677c81eedc3f6c', 'jowita.szef@gmail.com');
+INSERT INTO users VALUES ('super_prowadzacy', 'Bartosz', 'Algorytm', '9e301dfc77aeac8e3b6fc4c8fe28607d', 'bartosz.algorytm@[123.123.123.123]');
+INSERT INTO users VALUES ('niesuper_prowadzacy', 'Tycjan', 'Koder', 'd350d14e52114698fbf5206f9ae7e4d2', 'tycjan"()[]"koder-@domena');
+INSERT INTO users VALUES ('PilnyStudent', 'Evialina', 'Okejka', 'd7f3ec4fe96072168e6b39caae9705d6', 'ev2001@fajnapoczta.pl');
+INSERT INTO users VALUES ('pijepiwozamiastsieuczyc', 'Sergiusz', 'Znowuans', 'f12cbf3fe366d7f02304b5aff794a1ee', 'serg.ans@fajnapoczta.pl');
+INSERT INTO users VALUES ('I<3Mexico', 'Pablo Diego José Francisco', 'de Paula Juan Nepomuceno María de los Remedios Cipriano', '87bccbacffc1592e88f855b16f73faba', 'pablo@correos.mx');
 
-INSERT INTO administrators VALUES (0);
+INSERT INTO administrators VALUES (1);
 
-INSERT INTO contests VALUES (0, 'Kurs obsługi dziurkacza', 'FORTRAN 1977', false);
-INSERT INTO contests VALUES (1, 'Trudne problemy', 'C++ 2022', true);
-INSERT INTO contests VALUES (2, 'Boilerplate', 'JAVA 2024', true);
+INSERT INTO contests VALUES ('Kurs obsługi dziurkacza', 'FORTRAN 1977', false);
+INSERT INTO contests VALUES ('Trudne problemy', 'C++ 2022', true);
+INSERT INTO contests VALUES ('Boilerplate', 'JAVA 2024', true);
 
-INSERT INTO permissions_for_contests VALUES (0, 'MANAGE');
-INSERT INTO permissions_for_contests VALUES (1, 'SUBMIT');
+INSERT INTO permissions_for_contests VALUES ('MANAGE');
+INSERT INTO permissions_for_contests VALUES ('SUBMIT');
 
-INSERT INTO contests_permissions VALUES (1, 0, 1);
-INSERT INTO contests_permissions VALUES (2, 0, 2);
+INSERT INTO contests_permissions VALUES (2, 1, 2);
+INSERT INTO contests_permissions VALUES (3, 1, 3);
 
-INSERT INTO contests_permissions VALUES (3, 1, 1);
-INSERT INTO contests_permissions VALUES (3, 1, 2);
-INSERT INTO contests_permissions VALUES (4, 1, 1);
-INSERT INTO contests_permissions VALUES (4, 1, 2);
-INSERT INTO contests_permissions VALUES (5, 1, 1);
-INSERT INTO contests_permissions VALUES (5, 1, 2);
+INSERT INTO contests_permissions VALUES (4, 2, 2);
+INSERT INTO contests_permissions VALUES (4, 2, 3);
+INSERT INTO contests_permissions VALUES (5, 2, 2);
+INSERT INTO contests_permissions VALUES (5, 2, 3);
+INSERT INTO contests_permissions VALUES (6, 2, 2);
+INSERT INTO contests_permissions VALUES (6, 2, 3);
 
-INSERT INTO scoring_methods VALUES (0, 'LINEAR_TO_ZERO');
-INSERT INTO scoring_methods VALUES (1, 'LINEAR_TO_NEGATIVE');
+INSERT INTO scoring_methods VALUES ('LINEAR_TO_ZERO');
+INSERT INTO scoring_methods VALUES ('LINEAR_TO_NEGATIVE');
 
-INSERT INTO problems VALUES (0, 'Dziurkacz robi brrr', 'A', 0, 'archive.tanuki.uczelnia.pl/fortran/A', false, 0, 5, '1977-11-10', '1977-11-17', true, 0, 500);
-INSERT INTO problem_languages VALUES (0, 0);
-
-INSERT INTO problems VALUES (1, 'Bardzo trudne zadanie', 'A', 1, 'tanuki.uczelnia.pl/cpp2022/A', false, 0, 5, '2022-11-10', '2022-11-17', true, 0, 10000);
-INSERT INTO problems VALUES (2, 'Jeszcze trudniejsze zadanie', 'B', 1, 'tanuki.uczelnia.pl/cpp2022/B', false, 0, 5, '2022-12-15', '2022-12-22', true, 0, 10000);
-INSERT INTO problems VALUES (3, 'Żodyn z was nie zrobi', 'C', 1, 'tanuki.uczelnia.pl/cpp2022/C', false, 0, 5, '2023-01-07', '2023-01-14', true, 0, 10000);
-INSERT INTO problems VALUES (4, 'Generator warunków', 'D', 1, 'tanuki.uczelnia.pl/cpp2022/D', false, 0, 5, '2023-02-20', '2023-02-27', false, 0, 10000);
+INSERT INTO problems VALUES ('Dziurkacz robi brrr', 'A', 1, 'archive.tanuki.uczelnia.pl/fortran/A', false, 0, 5, '1977-11-10', '1977-11-17', true, 1, 500);
 INSERT INTO problem_languages VALUES (1, 1);
-INSERT INTO problem_languages VALUES (2, 1);
-INSERT INTO problem_languages VALUES (3, 1);
-INSERT INTO problem_languages VALUES (4, 1);
 
-INSERT INTO problems VALUES (5, 'Brzydka klasa', 'A', 2, 'tanuki.uczelnia.pl/java2024/A', true, 0, 5, '2025-11-10', '2025-11-17', true, 1, 50000);
+INSERT INTO problems VALUES ('Bardzo trudne zadanie', 'A', 2, 'tanuki.uczelnia.pl/cpp2022/A', false, 0, 5, '2022-11-10', '2022-11-17', true, 1, 10000);
+INSERT INTO problems VALUES ('Jeszcze trudniejsze zadanie', 'B', 2, 'tanuki.uczelnia.pl/cpp2022/B', false, 0, 5, '2022-12-15', '2022-12-22', true, 1, 10000);
+INSERT INTO problems VALUES ('Żodyn z was nie zrobi', 'C', 2, 'tanuki.uczelnia.pl/cpp2022/C', false, 0, 5, '2023-01-07', '2023-01-14', true, 1, 10000);
+INSERT INTO problems VALUES ('Generator warunków', 'D', 2, 'tanuki.uczelnia.pl/cpp2022/D', false, 0, 5, '2023-02-20', '2023-02-27', false, 1, 10000);
+INSERT INTO problem_languages VALUES (2, 2);
+INSERT INTO problem_languages VALUES (3, 2);
+INSERT INTO problem_languages VALUES (4, 2);
 INSERT INTO problem_languages VALUES (5, 2);
 
-INSERT INTO task_groups VALUES (0, 1, 'main', true);
-INSERT INTO tasks VALUES (0, 0, 'cpp/tests/A', 1, 10.0, 100000000, false);
-INSERT INTO task_groups VALUES (1, 2, 'main', true);
-INSERT INTO tasks VALUES (1, 1, 'cpp/tests/B', 1, 10.0, 100000000, false);
-INSERT INTO task_groups VALUES (2, 3, 'main', true);
-INSERT INTO tasks VALUES (2, 2, 'cpp/tests/C', 1, 10.0, 100000000, false);
-INSERT INTO task_groups VALUES (3, 4, 'main', true);
-INSERT INTO tasks VALUES (3, 3, 'cpp/tests/D', 1, 10.0, 100000000, false);
+INSERT INTO problems VALUES ('Brzydka klasa', 'A', 3, 'tanuki.uczelnia.pl/java2024/A', true, 0, 5, '2025-11-10', '2025-11-17', true, 2, 50000);
+INSERT INTO problem_languages VALUES (6, 3);
 
-INSERT INTO task_groups VALUES (4, 5, 'public tests', true);
-INSERT INTO tasks VALUES (4, 4, 'java/tests/A/00', 1, 10.0, 100000000, true);
-INSERT INTO tasks VALUES (5, 4, 'java/tests/A/01', 1, 10.0, 100000000, true);
-INSERT INTO tasks VALUES (6, 4, 'java/tests/A/02', 1, 10.0, 100000000, true);
+INSERT INTO task_groups VALUES (2, 'main', true);
+INSERT INTO tasks VALUES (1, 'cpp/tests/A', 1, 10.0, 100000000, false);
+INSERT INTO task_groups VALUES (3, 'main', true);
+INSERT INTO tasks VALUES (2, 'cpp/tests/B', 1, 10.0, 100000000, false);
+INSERT INTO task_groups VALUES (4, 'main', true);
+INSERT INTO tasks VALUES (3, 'cpp/tests/C', 1, 10.0, 100000000, false);
+INSERT INTO task_groups VALUES (5, 'main', true);
+INSERT INTO tasks VALUES (4, 'cpp/tests/D', 1, 10.0, 100000000, false);
 
-INSERT INTO task_groups VALUES (5, 5, 'private tests', true);
-INSERT INTO tasks VALUES (7, 5, 'java/tests/A/03', 1, 10.0, 100000000, false);
-INSERT INTO tasks VALUES (8, 5, 'java/tests/A/04', 1, 10.0, 100000000, false);
-INSERT INTO tasks VALUES (9, 5, 'java/tests/A/05', 1, 10.0, 100000000, false);
+INSERT INTO task_groups VALUES (6, 'public tests', true);
+INSERT INTO tasks VALUES (5, 'java/tests/A/00', 1, 10.0, 100000000, true);
+INSERT INTO tasks VALUES (5, 'java/tests/A/01', 1, 10.0, 100000000, true);
+INSERT INTO tasks VALUES (5, 'java/tests/A/02', 1, 10.0, 100000000, true);
 
-INSERT INTO statuses VALUES (0, 'OK', 'Program works correctly', 0);
-INSERT INTO statuses VALUES (1, 'ANS', 'Program produces wrong answer', 2);
-INSERT INTO statuses VALUES (2, 'MEM', 'Memory limit exceeded', 3);
-INSERT INTO statuses VALUES (3, 'RTE', 'Runtime error', 4);
-INSERT INTO statuses VALUES (4, 'CME', 'Compilation failed', 5);
-INSERT INTO statuses VALUES (5, 'TLE', 'Time limit exceeded', 1);
+INSERT INTO task_groups VALUES (6, 'private tests', true);
+INSERT INTO tasks VALUES (6, 'java/tests/A/03', 1, 10.0, 100000000, false);
+INSERT INTO tasks VALUES (6, 'java/tests/A/04', 1, 10.0, 100000000, false);
+INSERT INTO tasks VALUES (6, 'java/tests/A/05', 1, 10.0, 100000000, false);
 
-INSERT INTO submits VALUES (0, 'submitted_files/234jka.cpp', 3, 1, 1, '2022-11-01 16:24');
-INSERT INTO task_results VALUES (0, 0, 0, NULL, 3.0, 123456);
-INSERT INTO submit_results VALUES (0, 5.0, 0);
+INSERT INTO statuses VALUES ('OK', 'Program works correctly', 0);
+INSERT INTO statuses VALUES ('ANS', 'Program produces wrong answer', 2);
+INSERT INTO statuses VALUES ('MEM', 'Memory limit exceeded', 3);
+INSERT INTO statuses VALUES ('RTE', 'Runtime error', 4);
+INSERT INTO statuses VALUES ('CME', 'Compilation failed', 5);
+INSERT INTO statuses VALUES ('TLE', 'Time limit exceeded', 1);
 
-INSERT INTO submits VALUES (1, 'submitted_files/321aja.cpp', 3, 2, 1, '2022-11-01 21:38');
-INSERT INTO task_results VALUES (1, 0, 0, NULL, 3.0, 123456);
-INSERT INTO submit_results VALUES (1, 5.0, 0);
+INSERT INTO submits VALUES ('submitted_files/234jka.cpp', 3, 1, 1, '2022-11-01 16:24');
+INSERT INTO task_results VALUES (1, 1, 1, NULL, 3.0, 123456);
+INSERT INTO submit_results VALUES (1, 5.0, 1);
 
-INSERT INTO submits VALUES (2, 'submitted_files/adsf23.cpp', 3, 5, 2, '2025-11-01 16:24');
-INSERT INTO task_results VALUES (2, 4, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (2, 5, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (2, 6, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (2, 7, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (2, 8, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (2, 9, 0, NULL, 1.0, 123456);
-INSERT INTO submit_results VALUES (2, 5.0, 0);
+INSERT INTO submits VALUES ('submitted_files/321aja.cpp', 4, 3, 2, '2022-11-01 21:38');
+INSERT INTO task_results VALUES (2, 1, 1, NULL, 3.0, 123456);
+INSERT INTO submit_results VALUES (2, 5.0, 1);
 
-INSERT INTO submits VALUES (3, 'submitted_files/34254a.cpp', 4, 5, 2, '2025-11-01 16:24');
-INSERT INTO task_results VALUES (3, 4, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (3, 5, 5, NULL, 11.0, 123456);
-INSERT INTO task_results VALUES (3, 6, 1, 'for data 0 1 2 program returned 2 1 0, but it should be 0 1 2', 1.0, 123456);
-INSERT INTO task_results VALUES (3, 7, 0, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (3, 8, 3, NULL, 1.0, 123456);
-INSERT INTO task_results VALUES (3, 9, 0, NULL, 1.0, 123456);
-INSERT INTO submit_results VALUES (3, 0.0, 3);
+INSERT INTO submits VALUES ('submitted_files/adsf23.cpp', 4, 6, 3, '2025-11-01 16:24');
+INSERT INTO task_results VALUES (3, 5, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (3, 6, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (3, 7, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (3, 8, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (3, 9, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (3, 10, 1, NULL, 1.0, 123456);
+INSERT INTO submit_results VALUES (3, 5.0, 1);
+
+INSERT INTO submits VALUES ('submitted_files/34254a.cpp', 5, 6, 3, '2025-11-01 16:24');
+INSERT INTO task_results VALUES (4, 5, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (4, 6, 6, NULL, 11.0, 123456);
+INSERT INTO task_results VALUES (4, 7, 2, 'for data 0 1 2 program returned 2 1 0, but it should be 0 1 2', 1.0, 123456);
+INSERT INTO task_results VALUES (4, 8, 1, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (4, 9, 4, NULL, 1.0, 123456);
+INSERT INTO task_results VALUES (4, 10, 1, NULL, 1.0, 123456);
+INSERT INTO submit_results VALUES (4, 0.0, 4);
