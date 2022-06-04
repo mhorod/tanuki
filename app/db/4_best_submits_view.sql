6fda3362634576940b1c1d650bbbb289a738972b
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-06-04 22:09:36 +0200

    Views and comparators

---
+++ app/db/4_best_submits_view.sql
@@ -0,0 +1,10 @@
+CREATE VIEW newest_submits_in_problems AS
+SELECT a1.user_id, a1.problem_id, a1.id, result
+FROM all_submits a1
+WHERE a1.id = (
+    SELECT a2.id
+    FROM all_submits a2
+    WHERE a2.user_id = a1.user_id AND a2.problem_id = a1.problem_id
+    ORDER BY compare_submits(a2.*) DESC
+    LIMIT 1
+);
