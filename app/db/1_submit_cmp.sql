d84dc91f1ce9d48cf2b766abcedd8d507c740f07
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-06-05 10:03:43 +0200

    Fixed database files

--- app/db/1_submit_cmp.sql
+++ app/db/1_submit_cmp.sql
@@ -1,1 +1,10 @@
---nothing's here, currently
+CREATE OR REPLACE FUNCTION compare_submits(result INTEGER, submission_time TIMESTAMP)
+RETURNS TIMESTAMP AS
+$$
+    BEGIN
+            IF result = 1
+                 THEN RETURN TIMESTAMP 'infinity'; 
+            END IF;
+            RETURN submission_time;
+    END;
+$$ LANGUAGE plpgsql;
