9f2a098e815c2d16214838a49b5144a86740a77e
DominikMatuszek <dominik.matuszek@student.uj.edu.pl>
2022-06-07 21:32:04 +0200

    Fun with DB

--- app/db/2_all_submits_view.sql
+++ app/db/2_all_submits_view.sql
@@ -5,3 +5,16 @@ CREATE OR REPLACE VIEW all_submits AS
         WHERE sr.submit_id = s.id
     ) AS result, s.user_id, s.submission_time
     FROM submits s;
+
+CREATE OR REPLACE VIEW all_short_submits AS
+    SELECT s.id, p.id AS problem_id, p.short_name AS short_problem_name,
+        c.id AS contest_id, c.name AS contest_name, u.id AS user_id,
+        u.login AS user_login, l.name AS language_name, 
+        s.submission_time, sr.status
+    FROM submits s
+    JOIN problems p ON s.problem_id = p.id
+    JOIN contests c ON c.id = p.contest_id
+    JOIN users u ON u.id = s.user_id
+    JOIN languages l ON l.id = s.language_id
+    JOIN submit_results sr ON sr.submit_id = s.id
+    ORDER BY s.submission_time DESC;
--- app/db/6_user_score.sql
+++ app/db/6_user_score.sql
@@ -9,3 +9,12 @@ CREATE OR REPLACE VIEW problem_scores AS
         st.name != 'REJ'
     GROUP BY
         1, 2; 
+
+CREATE OR REPLACE VIEW task_scores AS 
+    SELECT 
+        tr.submit_id, tr.task_id, tr.status_id, tr.points AS points,
+        tr.summary, tr.execution_time, tr.used_memory,
+        t.task_group, t.name, t.test_uri, t.points AS max_points,
+        t.memory_limit, t.show_output
+    FROM task_results tr
+    JOIN tasks t ON tr.task_id = t.id;
